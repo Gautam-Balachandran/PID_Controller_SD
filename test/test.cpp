cce24ccc9f1f55d04ea20b6067495286a76534bb
Gautam Balachandran <47335017+Gautam-Balachandran@users.noreply.github.com>
2019-09-25 13:49:39 -0400

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -27,12 +27,12 @@ TEST(PID_Controller_Test, CheckValuePass) {
 /**
  * @brief Test case that checks if the computed value is different
  */
-TEST(PID_Controller_Test, CheckValueFail) {
-  PID_Controller pid_controller;
-  double expect_fail = 140;
-  double val = pid_controller.compute_velocity(10.0,24.3);
-  EXPECT_NEAR(expect_fail,val,0.01);// Should fail
-}
+// TEST(PID_Controller_Test, CheckValueFail) {
+//   PID_Controller pid_controller;
+//   double expect_fail = 140;
+//   double val = pid_controller.compute_velocity(10.0,24.3);
+//   EXPECT_NEAR(expect_fail,val,0.01);// Should fail
+// }
 
 /**
  * @brief Test case to check if the computed value is type double
@@ -47,9 +47,9 @@ TEST(PID_Controller_Test, checkDoublePass) {
 /**
  * @brief Test case to check if the computed value is not type int
  */
-TEST(PID_Controller_Test, checkDoubleFail) {
-  PID_Controller pid_controller;
-  double val = pid_controller.compute_velocity(10.0,24.3);
-  // For failure check
-  EXPECT_EQ(typeid(val), typeid(int));
-}
+// TEST(PID_Controller_Test, checkDoubleFail) {
+//   PID_Controller pid_controller;
+//   double val = pid_controller.compute_velocity(10.0,24.3);
+//   // For failure check
+//   EXPECT_EQ(typeid(val), typeid(int));
+// }
