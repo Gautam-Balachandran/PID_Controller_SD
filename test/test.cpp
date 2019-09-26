e7912287f126bb60a33fc068702790857e18f803
Gautam Balachandran <47335017+Gautam-Balachandran@users.noreply.github.com>
2019-09-26 17:16:30 -0400

    Converting to Camel Case

--- test/test.cpp
+++ test/test.cpp
@@ -17,29 +17,29 @@
 /**
  * @brief Test case that checks if the computed value is correct
  */
-TEST(PID_Controller_Test, CheckValuePass) {
-  PID_Controller pid_controller;
-  double expect_pass = 140.14;
-  double val = pid_controller.compute_velocity(10.0,24.3);
-  EXPECT_NEAR(expect_pass,val,0.01);// Should Pass
+TEST(PIDControllerTest, CheckValuePass) {
+  PID_Controller pidController;
+  double expectPass = 140.14;
+  double val = pidController.compute_velocity(10.0,24.3);
+  EXPECT_NEAR(expectPass,val,0.01);// Should Pass
 }
 
 /**
  * @brief Test case that checks if the computed value is different
  */
-// TEST(PID_Controller_Test, CheckValueFail) {
-//   PID_Controller pid_controller;
-//   double expect_fail = 140;
-//   double val = pid_controller.compute_velocity(10.0,24.3);
-//   EXPECT_NEAR(expect_fail,val,0.01);// Should fail
+// TEST(PIDControllerTest, CheckValueFail) {
+//   PID_Controller pidController;
+//   double expectFail = 140;
+//   double val = pidController.compute_velocity(10.0,24.3);
+//   EXPECT_NEAR(expectFail,val,0.01);// Should fail
 // }
 
 /**
  * @brief Test case to check if the computed value is type double
  */
-TEST(PID_Controller_Test, checkDoublePass) {
-  PID_Controller pid_controller;
-  double val = pid_controller.compute_velocity(10.0,24.3);
+TEST(PIDControllerTest, checkDoublePass) {
+  PID_Controller pidController;
+  double val = pidController.compute_velocity(10.0,24.3);
   // For Pass check
   EXPECT_EQ(typeid(val), typeid(double));
 }
@@ -47,9 +47,9 @@ TEST(PID_Controller_Test, checkDoublePass) {
 /**
  * @brief Test case to check if the computed value is not type int
  */
-// TEST(PID_Controller_Test, checkDoubleFail) {
-//   PID_Controller pid_controller;
-//   double val = pid_controller.compute_velocity(10.0,24.3);
+// TEST(PIDControllerTest, checkDoubleFail) {
+//   PID_Controller pidController;
+//   double val = pidController.compute_velocity(10.0,24.3);
 //   // For failure check
 //   EXPECT_EQ(typeid(val), typeid(int));
 // }
