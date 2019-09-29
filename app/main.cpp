adfc952f7b674e8ad75a44988a47aea865b64e34
Gautam Balachandran <47335017+Gautam-Balachandran@users.noreply.github.com>
2019-09-29 14:25:32 -0400

    Converting to Camel Case

--- app/main.cpp
+++ app/main.cpp
@@ -11,8 +11,8 @@
 int main()
 {
   // Creates pid_controller object
-  PID_Controller pidController;
-  double val = (double)pidController.compute_velocity(10.0,24.3);
+  PIDController pidController;
+  double val = (double)pidController.computeVelocity(10.0,24.3);
   // prints value
   std::cout<<"Value : "<<val<<std::endl;
   return 0;
